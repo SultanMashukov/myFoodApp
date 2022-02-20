fa03f8e89500ea1b92c80279e11380c9f5ad4e83
SultanMashukov <mashukov-sultan@yandex.ru>
2022-02-20 09:48:38 +0300

    added react-redux, buisness layer started, app has been wraped by store Provider, catalog list binded to store

--- src/store/rootReduser.js
+++ src/store/rootReduser.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import catalogReducer from './slices/sliceCatalog'
+import catalogReducer from './slices/sliceCatalog';
+import basketReducer from "./slices/sliceBasket";
 
 export default  configureStore({
     reducer:{
         catalog: catalogReducer,
+        basker: basketReducer,
     }
 })
