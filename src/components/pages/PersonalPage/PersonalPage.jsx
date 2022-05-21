c59b6918ad4c165d008b544a38d3ba57dac087c2
SultanMashukov <mashukov-sultan@yandex.ru>
2022-05-21 12:09:26 +0300

    PersonalPage classnames

--- src/components/pages/PersonalPage/PersonalPage.jsx
+++ src/components/pages/PersonalPage/PersonalPage.jsx
@@ -4,43 +4,43 @@ import './PersonalPage.scss';
 
 const PersonalPage = (props) => {
 	return (
-		<div class="personal">
-			<div class="pageHeader">
-					<div class="pageHeader__title">
+		<div className="personal">
+			<div className="pageHeader">
+					<div className="pageHeader__title">
 							Личный кабинет
 					</div>
 					
 			</div>
-			<div class="personal__row">
-					<div class="personal__item">
-							<a href="" class="personal__item-link">
-									<span class="personal__item-icon">
-											<i class="fal fa-info"></i>
+			<div className="personal__row">
+					<div className="personal__item">
+							<a href="" className="personal__item-link">
+									<span className="personal__item-icon">
+											<i className="fal fa-info"></i>
 									</span>Мои данные</a>
 					</div>
-					<div class="personal__item">
-							<a href="" class="personal__item-link">
-									<span class="personal__item-icon">
-											<i class="far fa-map-marked"></i>
+					<div className="personal__item">
+							<a href="" className="personal__item-link">
+									<span className="personal__item-icon">
+											<i className="far fa-map-marked"></i>
 									</span>Мои адреса</a>
 					</div>
-					<div class="personal__item">
+					<div className="personal__item">
 							<NavLink to='orders/' className='personal__item-link'>
-								<span class="personal__item-icon">
-									<i class="fal fa-history"></i>
+								<span className="personal__item-icon">
+									<i className="fal fa-history"></i>
 								</span>Мои заказы
 							</NavLink>
 					</div>
-					<div class="personal__item">
-							<a href="" class="personal__item-link">
-									<span class="personal__item-icon">
-											<i class="fas fa-heart"></i>
+					<div className="personal__item">
+							<a href="" className="personal__item-link">
+									<span className="personal__item-icon">
+											<i className="fas fa-heart"></i>
 									</span>Избранные блюда</a>
 					</div>
-					<div class="personal__item">
-							<a href="" class="personal__item-link">
-									<span class="personal__item-icon">
-											<i class="fal fa-weight-hanging"></i>
+					<div className="personal__item">
+							<a href="" className="personal__item-link">
+									<span className="personal__item-icon">
+											<i className="fal fa-weight-hanging"></i>
 									</span>Мои наборы</a>
 					</div>
 			</div>
