f87a8de6a5c45a1899d6592bdae122467289799d
SultanMashukov <mashukov-sultan@yandex.ru>
2022-05-24 13:17:17 +0300

    removed unused imports

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,10 +1,9 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import CatalogMenu from './CatalogMenu/CatalogMenu';
 import './CatalogPage.scss';
 import CatalogList from './CatalogList/CatalogList';
-import { useSelector, useDispatch } from 'react-redux';
-import { toggleModal,setProductModalId, fetchCatalogItems, setNameFilter } from 'store/slices/sliceCatalog';
-import SearchString from 'components/SearchString/SearchString';
+import { useDispatch } from 'react-redux';
+import { toggleModal,setProductModalId, fetchCatalogItems } from 'store/slices/sliceCatalog';
 import CatalogPicker from './CatalogPicker/CatalogPicker';
 import CatalogSearchString from './CatalogSearchString/CatalogSearchString';
 
