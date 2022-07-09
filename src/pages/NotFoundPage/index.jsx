bc3a48e13224c0897366f34ae0bb60c69445e902
SultanMashukov <mashukov-sultan@yandex.ru>
2022-07-09 08:19:23 +0300

    404 page style changed

--- src/pages/NotFoundPage/index.jsx
+++ src/pages/NotFoundPage/index.jsx
@@ -5,17 +5,25 @@ import { NavLink } from 'react-router-dom';
 const NotFoundPage = (props) => {
 	return (
 		<div className="notFoundPage">
-			<div className="notFoundPage__error">
-				404
+			<div className="pageHeader">
+				<div className="pageHeader__title">
+						Упс,ошибка :(
+				</div>
 			</div>
-			<div className="notFoundPage__text">
-				По вашему запросу ничего не найдено. 
-				<br />
-				Возможно неправильно набран адрес или страница была удалена.
+			<div className="notFoundPage__inner">
+				<div className="notFoundPage__error">
+					404
+				</div>
+				<div className="notFoundPage__text">
+					<b>По вашему запросу ничего не найдено.</b> 
+					<br />
+					<br />
+					Возможно неправильно набран адрес или страница была удалена.
+				</div>
+				<NavLink to='/catalog/' className="notFoundPage__link">
+					В меню <i class="fal fa-utensils"></i>
+				</NavLink>
 			</div>
-			<NavLink to='/catalog/' className="notFoundPage__link">
-				В меню <i class="fal fa-utensils"></i>
-			</NavLink>
 		</div>
 	) 
 };
