96e0696772429533b3e26af3b396d7e5b4d5d362
SultanMashukov <mashukov-sultan@yandex.ru>
2022-07-14 17:45:15 +0300

    in utils added function for get cookie by name

--- src/utils/index.js
+++ src/utils/index.js
@@ -43,3 +43,10 @@ export function debounce(f, ms) {
     };
 
 }
+
+export function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
