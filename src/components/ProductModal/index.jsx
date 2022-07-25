14a59b104792dca07ca2530d5938c45d05e47b4f
SultanMashukov <mashukov-sultan@yandex.ru>
2022-07-25 14:19:30 +0300

    product modal has been render optimized

--- src/components/ProductModal/index.jsx
+++ src/components/ProductModal/index.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import './styles.scss';
-import Counter from 'components/Counter';
 import ProductSlider from './ProductSlider';
 import { addItemToBasket } from 'store/slices/sliceBasket';
+import ProductOptions from './ProductOptions';
+import ProductCounter from './ProductCounter';
 
 const ProductModal = ({toggleProductModal, productData}) => {
-    const [productCount, setProductCount] = useState('1');
-    const [productOptions, setProductOptions] = useState(productData.options.map((elem) => ({name: elem, active: true})));
-    const dispatch = useDispatch();
     
-    const changeProductCount = (newCount) => {
-        if(newCount>=1){
-            setProductCount(newCount);
-        }
+    const dispatch = useDispatch();
+    const refCountAndOption = useRef({
+        count: 1,
+        options: productData.options.map((elem) => ({name: elem, active: true}))
+    })
+    
+    const changeCount = (newCount) => {
+        refCountAndOption.current.count = newCount;
     }
-
-    const toggleProductOption = (optionName) => {
-        setProductOptions((prevState) => {
-            return prevState.map(option => option.name === optionName ? {...option, active : !option.active} : option);
-        })
+    const changeOption = (optionName) => {
+        let elemId = refCountAndOption.current.options.findIndex(el => el.name === optionName);
+        refCountAndOption.current.options[elemId].active = !refCountAndOption.current.options[elemId].active
     }
 
     const addProductToBasket = () => {
@@ -27,9 +27,9 @@ const ProductModal = ({toggleProductModal, productData}) => {
             productId: productData.id,
             name: productData.name,
             image: productData.images[0],
-            count: productCount,
+            count: refCountAndOption.current.count,
             price: productData.price,
-            options:productOptions,
+            options: refCountAndOption.current.options,
             type: productData.food_type.code,
         }
         dispatch(addItemToBasket(dataForBasket));
@@ -42,43 +42,23 @@ const ProductModal = ({toggleProductModal, productData}) => {
             toggleProductModal(false);
 		}}>
 			<div className='product-modal__content' >
-				
-			<div className="product-modal__close" onClick={(e) => toggleProductModal()}>
-                <i className="fal fa-times"></i>
-            </div>
-            <ProductSlider imageURLs={productData.images }/>
-			<div className="product-modal__info">
-                <div className="product-modal__name">{productData.name}</div>
-                <div className="product-modal__description">
-                    {productData.description}
+                <div className="product-modal__close" onClick={(e) => toggleProductModal()}>
+                    <i className="fal fa-times"></i>
                 </div>
-                
-                <div className="product-modal__options">
-                    {
-                        productOptions.map((option, id) => (
-                            <div className="product-modal__options-item" key={id} onClick={() => toggleProductOption(option.name)}>
-                                <input className="product-modal__options-checkbox" 
-                                type="checkbox" 
-                                checked={option.active}
-                                onChange={(e)=> toggleProductOption(option.name)}
-                                />
-                                <div className="product-modal__options-name">{option.name}</div>
-                            </div>
-                        ))
-                    }
-                    
-                </div>
-                
-                <div className="product-modal__order">
-                    <div className="product-modal__price">
-                        {productData.price * productCount} <i className="fal fa-ruble-sign"></i>
+                <ProductSlider imageURLs={productData.images }/>
+                <div className="product-modal__info">
+                    <div className="product-modal__name">{productData.name}</div>
+                    <div className="product-modal__description">
+                        {productData.description}
                     </div>
-                    <Counter initCount={productCount} changeCount={changeProductCount}/>
-                    <button className="product-modal__submit" onClick={() => addProductToBasket()}><i className="fal fa-shopping-basket"></i></button>
+                    <ProductOptions options={productData.options} changeOption={changeOption}/>
+                    <div className="product-modal__order">
+                        
+                        <ProductCounter changeCount={changeCount} price={productData.price}/>
+                        
+                        <button className="product-modal__submit" onClick={() => addProductToBasket()}><i className="fal fa-shopping-basket"></i></button>
+                    </div>
                 </div>
-            </div>
-
-
 			</div>
 		</div>
 	) 
